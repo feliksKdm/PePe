3765f40bf74b1f2826111d4d04b042c42d64b6a3
Feliks A <feliksaltymysov@gmail.com>
2025-09-02 21:40:15 -0400

    fc

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+
+function App() {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      <Navbar />
+      {/*main section*/}
+      {/*footer*/}
+    </div>
+  )
+}
+
+export default App
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <div className="fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-zinc-900/20">
+      <div className="mx-auto c-space max-w-7xl">
+        <div className="flex items-center justify-between p-2 md:p-0">
+          <a className="text-xl font-bold transition-colors text-neutral-400 hover:text-white">flex</a>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
