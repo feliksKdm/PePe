654ecc1cf36efbde2802b8951e204511a67cbddd
Feliks A <feliksaltymysov@gmail.com>
2025-09-10 18:38:07 -0400

    background

--- src/components/ParalaxBackground.jsx
+++ src/components/ParalaxBackground.jsx
@@ -1,10 +1,22 @@
+import { section } from 'motion/react-client';
+import React from 'react';
 
 const ParalaxBackground = () => {
     return (
-        <div>
-                       
-        </div>
-    );
-}
+        <section className='absolute inset-0 bg-black/40'> 
+        <div className='realtive h-screen overflow-hidden'>
+            
+            <div 
+            className='absolute inset-0 w-full h-screen -z-50'
+            style={{ backgroundImage: "url(/assets/sky.jpg", }}>
 
-export default ParalaxBackground;
+        </div>
+        
+        </div> 
+        
+
+        </section>
+    );
+};
+
+export { ParalaxBackground };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,22 +1,30 @@
 import { div } from "motion/react-client"
 import { FlipWords } from "../components/FlipWords"
-import { ParalaxBackground} from "../components/ParalaxBackground"
+import { ParalaxBackground } from "../components/ParalaxBackground"
 
 function Hero () {
   return ( 
-    <div className="min-h-screen flex flex-col  items-center text-center c-space rounded-3xl z-10 pt-20 md:pt-40">
-      <h1 className="text-4xl font-bold mb-4 text-neutral-300 md:text-7xl">Hi, im Feliks</h1>
-      <h2 
-      className="text-3xl text-neutral-300 text-center md:text-6xl">Building</h2>
-      <div>
-        <FlipWords 
-          words={["websites", "apps", "idk", "stuff :O", "I can do Big Data actually"]}
-          duration={2000}
-          className="text-3xl font-bold text-neutral-300 p-3 md:text-6xl" 
-        />
-      <ParalaxBackground />
-      </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    
+    <div className="min-h-screen flex flex-col  items-center text-center c-space rounded-3xl z-100 pt-20 md:pt-40">
+      <div className="z-10 text-white">
+        <h1 className=" text-4xl font-bold mb-4 md:text-7xl">Hi, im Feliks</h1>
+        <h2 
+        className="text-3xl text-center md:text-6xl">Building</h2>
+        
+        <div>
+          <FlipWords 
+            words={["websites", "apps", "idk", "stuff :O", "I can do Big Data actually"]}
+            duration={2000}
+            className="text-3xl font-bold  p-3 md:text-6xl" 
+          />
+        </div>
+      </div>
+      
+      <ParalaxBackground />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
     </div>
+
+    
+
   )
 }
 
