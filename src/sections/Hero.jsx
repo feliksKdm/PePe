de2c0d883758cfdace8e108c02f5cb315a41ded0
Feliks A <feliksaltymysov@gmail.com>
2025-09-12 20:08:53 -0400

    Rig

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,16 @@
 import { div } from "motion/react-client"
 import { FlipWords } from "../components/FlipWords"
 import { ParalaxBackground } from "../components/ParalaxBackground"
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from "@react-three/drei"
 import { GirlSpace } from "../components/GirlSpace"
+import { use } from "react"
+import { easing } from "maath"
 
 function Hero () {
   return ( 
     
-    <section className="min-h-screen flex flex-col  items-center justify-center text-center c-space rounded-3xl z-100 pt-5 md:pt-10 ">
+    <section className="min-h-screen flex flex-col  items-center justify-start text-center c-space rounded-3xl z-100 pt-20 md:pt-40 ">
       <div className="z-10  text-white text-4xl font-bold [text-shadow:2px_2px_6px_rgba(0,0,0,0.4)]">
         <h1 className=" text-4xl font-bold mb-4 md:text-7xl">Hi, im Feliks</h1>
         <h2 
@@ -28,13 +30,10 @@ function Hero () {
       <figure className="inset-0 absolute z-1 "
       style={{ width: "100vw", height: "100vh"}}
       >
-
-        <Canvas >
-
+        <Canvas camera={{ fov: 50 }}>
           <GirlSpace 
-          position={[0, -6.3, 0]} scale={0.065}
-          rotation={[0, Math.PI / 4, 0]}
-          castShadow
+          position={ [0, -6.3, 0]} scale={ 0.065}
+          rotation={ [-0.2, Math.PI / 6, 0]}
           />
 
           <OrbitControls 
@@ -42,6 +41,7 @@ function Hero () {
           maxPolarAngle={Math.PI / 2} 
           />
 
+          <Rig />
         </Canvas>
 
       </figure>
@@ -50,6 +50,16 @@ function Hero () {
 
     
 
+  )
+}
+
+function Rig() {
+  return useFrame((state, delta) => {
+    easing.damp3(state.camera.position, 
+      [state.mouse.x / 5, 1 + state.mouse.y / 5, 3], 0.5, 
+      delta
+    )
+  }
   )
 }
 
